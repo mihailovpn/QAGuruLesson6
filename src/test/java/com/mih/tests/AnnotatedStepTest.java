e6f10f18bb8286ad9e25339d2e09bc390b4bf29a
Pavel Mikhaylov <pavel.mikhaylov@simbirsoft.com>
2021-09-23 00:39:26 +0300

    Added attachments

--- src/test/java/com/mih/tests/AnnotatedStepTest.java
+++ src/test/java/com/mih/tests/AnnotatedStepTest.java
@@ -1,8 +1,14 @@
 package com.mih.tests;
 
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-
+@Feature("Menu")
+@Owner("Mikhailov")
 public class AnnotatedStepTest {
     public static final String  PAGE = "https://github.com/";
     public static final String  REPOSITORY = "eroshenkoam/allure-example";
@@ -10,6 +16,8 @@ public class AnnotatedStepTest {
 
 
     @Test
+    @DisplayName("Проверка меню")
+    @Severity(SeverityLevel.MINOR)
     void checkIssueOnGithub() {
 
         WebSteps steps = new WebSteps();
--- src/test/java/com/mih/tests/WebSteps.java
+++ src/test/java/com/mih/tests/WebSteps.java
@@ -1,7 +1,10 @@
 package com.mih.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import io.qameta.allure.Attachment;
 import io.qameta.allure.Step;
+import org.openqa.selenium.OutputType;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byPartialLinkText;
@@ -19,14 +22,25 @@ public class WebSteps {
     @Step("Find repository {repository}")
     public void searchForRepository(String repository) {
         $(".header-search-input").setValue(repository).pressEnter();
+        takeScreenshot();
     }
     @Step("Open repository {repository}")
     public void goToRepository(String repository) {
         $(linkText(repository)).click();
+        takeScreenshot();
     }
     @Step("Check menu {menuName}")
     public void shouldHaveMenu(String menuName) {
         $(byPartialLinkText(menuName)).should(visible);
+        takeScreenshot();
     }
+
+    @Attachment(value = "Скриншот", type = "image/png")
+    public byte[] takeScreenshot() {
+        return Selenide.screenshot(OutputType.BYTES);
+    }
+
 }
+
+
 
